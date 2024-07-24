5754074d8d13e962a29352bfc7065c314741b0b6
Alejandro de Pablos Lopez <alejandro.depablos@sngular.com>
2024-07-24 18:21:09 -0400

    Configuring to point based on env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Expense, ExpenseInput } from '../types';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
